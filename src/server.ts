9207daef1d22c8b29b3f0808312f324ebcfa5063
Guntur KH <guntur.kh@gmail.com>
2024-08-19 00:13:52 +0700

    feat: telegram basic command

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,12 @@ import { userRouter } from "@/api/user/userRouter";
 import errorHandler from "@/common/middleware/errorHandler";
 import rateLimiter from "@/common/middleware/rateLimiter";
 import requestLogger from "@/common/middleware/requestLogger";
+import axios from "axios";
 import { env } from "@/common/utils/envConfig";
+
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const logger = pino({ name: "server start" });
 const app: Express = express();
@@ -36,5 +41,30 @@ app.use(openAPIRouter);
 
 // Error handlers
 app.use(errorHandler());
+
+bot.launch();
+bot.command("start", (ctx: any) => {
+  console.log(ctx.from);
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    "Hello there! Welcome to the Code Capsules telegram bot.nI respond to /ethereum. Please try it",
+    {}
+  );
+});
+
+bot.command("ethereum", (ctx: any) => {
+  var rate;
+  console.log(ctx.from);
+  axios
+    .get(
+      `https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`
+    )
+    .then((response) => {
+      console.log(response.data);
+      rate = response.data.ethereum;
+      const message = `Hello, today the ethereum price is ${rate.usd}USD`;
+      bot.telegram.sendMessage(ctx.chat.id, message, {});
+    });
+});
 
 export { app, logger };
